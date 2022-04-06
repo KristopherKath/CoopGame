3ea683da5e3ee88e271c6fe709e22ae2c4b56ef4
kris <Kristopher.Kath@gmail.com>
2022-04-06 09:53:52 -0700

    Created GameMode Wave states. Hooked BP implementable event to Bot Spawning EQS system

--- Source/CoopGame/Public/SGameMode.h
+++ Source/CoopGame/Public/SGameMode.h
@@ -14,4 +14,40 @@ class COOPGAME_API ASGameMode : public AGameModeBase
 {
 	GENERATED_BODY()
 	
+protected:
+
+	FTimerHandle TimerHandle_BotSpawner;
+
+	//number of bots to spawn in current wave
+	int32 NumBotsToSpawn;
+
+	int32 WaveCount;
+
+	UPROPERTY(EditDefaultsOnly, Category = "GameMode")
+	float TimeBetweenWaves;
+
+protected:
+
+	// Hook for BP to spawn a single bot
+	UFUNCTION(BlueprintImplementableEvent, Category = "GameMode")
+	void SpawnNewBot();
+
+	void SpawnBotTimerElapsed();
+
+	// Start Spawning bots
+	void StartWave();
+
+	//Stop Spawning bots
+	void EndWave();
+
+	// Set timer for the next start wave
+	void PrepareForNextWave();
+
+public:
+
+	ASGameMode();
+
+
+	virtual void StartPlay() override;
+
 };
